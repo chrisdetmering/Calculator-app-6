d725563983157da9486fd7f7bf0cd0a6f80a8bd6
James Hirschfelder <james.hirschfelder@gmail.com>
2021-05-19 11:41:58 -0700

    reorganized, breaking into single purpose functions

--- calculator.js
+++ calculator.js
@@ -4,57 +4,59 @@ let firstValue = ""
 let secondValue = ""
 let memoryValue = ""
 
-function renderDisplay () {
-  document.getElementById('new-input').value=currentValue;
-  document.getElementById('output').value=`${firstValue} ${savedOperator} ${secondValue}`;
-}
-
 document.querySelectorAll(".numeric").forEach(numberButton => {
   numberButton.addEventListener('click', event => {
-    validate();
-    switch(event.target.id) {
-      case "1": currentValue += "1"; break;
-      case "2": currentValue += "2"; break;
-      case "3": currentValue += "3"; break;
-      case "4": currentValue += "4"; break;
-      case "5": currentValue += "5"; break;
-      case "6": currentValue += "6"; break;
-      case "7": currentValue += "7"; break;
-      case "8": currentValue += "8"; break;
-      case "8": currentValue += "8"; break;
-      case "9": currentValue += "9"; break;
-      case "0":
-        currentValue == "" ? console.log("Error: No leading zero") : currentValue += "0";
-        break;
-      case ".":
-        currentValue.indexOf('.') >= 0 ? console.log("Error: Already contains decimal") : currentValue += ".";
-        break;
-    }
-    renderDisplay()
+    const numberID=event.target.id;
+    //validateForNumbers
+    pressNumberButton(numberID);
+    renderDisplay() 
  });
 });
 
-function validate() {
-  //account for previous equation state
+//function validateforNumbers
+
+function pressNumberButton(numberID) {
+  switch(numberID) {
+    case "1": currentValue += "1"; break;
+    case "2": currentValue += "2"; break;
+    case "3": currentValue += "3"; break;
+    case "4": currentValue += "4"; break;
+    case "5": currentValue += "5"; break;
+    case "6": currentValue += "6"; break;
+    case "7": currentValue += "7"; break;
+    case "8": currentValue += "8"; break;
+    case "8": currentValue += "8"; break;
+    case "9": currentValue += "9"; break;
+    case "0":
+      currentValue == "" ? console.log("Error: No leading zero") : currentValue += "0";
+      break;
+    case ".":
+      currentValue.indexOf('.') >= 0 ? console.log("Error: Already contains decimal") : currentValue += ".";
+      break;
+  }
 }
 
 document.querySelectorAll(".operator").forEach(operatorButton => {
   operatorButton.addEventListener('click', event => {
-    if (currentValue=="0" || currentValue=="") {
-      console.log('Error: operator requires input value')
-    } else if (firstValue!="") {
-      console.log("I'm calculating!")
-      //calculate()
-    } else {
-      const newOperator=event.target.id
-      addOperator(newOperator)
-    }
+    const operatorID=event.target.id;
+    validateforOperators(operatorID);
+    pressOperatorButton(operatorID);
     renderDisplay()
   });
 });
 
-function addOperator(newOperator) {
-  switch(newOperator) {
+function validateforOperators(inputID){
+  if (currentValue=="0" || currentValue=="") {
+    console.log('Error: operator requires input value')
+  } 
+  if (firstValue!="") {
+    console.log("I'm calculating!")
+    calculate()
+  } 
+}
+
+function pressOperatorButton(operatorID) {
+  switch(operatorID) {
     case "add": savedOperator = "+"; break;
     case "subtract": savedOperator = "-"; break;
     case "multiply": savedOperator = "x"; break;
@@ -64,33 +66,6 @@ function addOperator(newOperator) {
   firstValue=currentValue;
   currentValue="";
 }
-
-//function calculate() {}
-
-document.querySelectorAll(".action").forEach(actionButton => {
-  actionButton.addEventListener('click', event => {
-    if (currentValue !="") {
-    switch(event.target.id) {
-      case "fraction": firstValue=`1/${currentValue}`;
-       currentValue = 1 / parseInt(currentValue);
-       currentValue=currentValue.toString(); 
-        break;
-      case "square": firstValue=`sqr(${currentValue})`;
-      currentValue = parseInt(currentValue)**2; 
-      currentValue=currentValue.toString();
-        break;
-      case "square-root": firstValue=`sqrt(${currentValue})`;
-      currentValue = Math.sqrt(parseInt(currentValue)); 
-      currentValue=currentValue.toString();
-      if (currentValue=="NaN") {console.log("Cannot take square root of a negative number")}
-        break;
-      case "positive-negative": currentValue.search("-")!=-1 ?  currentValue=currentValue.substring(1) : currentValue=`-${currentValue}`;
-        break;
-    }
-    renderDisplay()
-  }
- });
-});
 
 document.getElementById("backspace").addEventListener('click', () => {
   currentValue=currentValue.substring(0, currentValue.length - 1);
@@ -107,21 +82,60 @@ document.getElementById("clear-content").addEventListener('click', () => {
   renderDisplay()
 });
 
+document.getElementById("equals").addEventListener('click', () => {
+  calculate();
+  renderDisplay();
+});
 
+/*function resetScreen() {
+  if (currentValue==="NaN" || currentValue==="Infinity") {
+    currentValue="Error";
+  }
+  if (secondValue!="")
+  {
+    currentValue = ""; secondValue = "";
+  }
+}*/
 
+function calculate() {
+  secondValue=currentValue;
+  switch(savedOperator) {
+    case "+": console.log("hi");
+    currentValue=parseInt(firstValue)+parseInt(secondValue); break;
+    case "-": currentValue=parseInt(firstValue)-parseInt(secondValue);  break;
+    case "x": currentValue=parseInt(firstValue)*parseInt(secondValue); break;
+    case "/": currentValue=parseInt(firstValue)/parseInt(secondValue); break;
+    case "^": currentValue=parseInt(firstValue)**parseInt(secondValue);break;
+  }
+  secondValue=`${secondValue} =`;
+}
 
+document.querySelectorAll(".action").forEach(actionButton => {
+  actionButton.addEventListener('click', event => {
+    if (currentValue !="") {
+    switch(event.target.id) {
+      case "fraction": secondValue=`1/${currentValue}`;
+       currentValue = 1 / parseInt(currentValue);
+       currentValue=currentValue.toString(); 
+        break;
+      case "square": secondValue=`sqr(${currentValue})`;
+      currentValue = parseInt(currentValue)**2; 
+      currentValue=currentValue.toString();
+        break;
+      case "square-root": secondValue=`sqrt(${currentValue})`;
+      currentValue = Math.sqrt(parseInt(currentValue)); 
+      currentValue=currentValue.toString();
+      if (currentValue=="NaN") {console.log("Cannot take square root of a negative number")}
+        break;
+      case "positive-negative": currentValue.search("-")!=-1 ?  currentValue=currentValue.substring(1) : currentValue=`-${currentValue}`;
+        break;
+    }
+    renderDisplay()
+  }
+ });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function renderDisplay () {
+  document.getElementById('new-input').value=currentValue;
+  document.getElementById('output').value=`${firstValue} ${savedOperator} ${secondValue}`;
+}
